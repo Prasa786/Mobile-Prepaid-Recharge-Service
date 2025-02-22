a99336807ec3cdf1262385e44a4aeb2050ac580b
Prasanna <prasannarps786@gmail.com>
2025-02-23 00:13:09 +0530

    first commit

--- Project Techm/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let myCarousel = new bootstrap.Carousel(document.querySelector("#bannerCarousel"), {
-        interval: 4000
-    });
-});
